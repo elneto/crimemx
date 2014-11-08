580c4a13abe1c1eb849552db8a96644183afbe9f
Ernesto Araiza <yo@ernestoaraiza.com>
2014-11-08 17:47:28 -0500

    Removed D3 Force Layout. Using pure Kartograph

--- js/demers.js
+++ js/demers.js
@@ -32,10 +32,6 @@
         }
         return +max;
       }
-
-       //for the colors 
-      var colHi = chroma.hex('#FA5050'), //homicides red
-          colLow = chroma.hex("#5DBEAB");
       
       //the SVG main demers map    
       var margin = {top: 0, right: 0, bottom: 0, left: 0},
@@ -50,15 +46,6 @@
                 .scale(1700)
                 .translate([width / 2, height / 2])
                 ;
-
-      var force = d3.layout.force()
-          .charge(0)
-          .gravity(0)
-          .size([width, height]);
-
-      var svg = d3.select("#demers").append("svg")
-          .attr("width", width)
-          .attr("height", height);
 
       var xSlider = d3.time.scale()
           .domain([new Date(1997, 0, 1), new Date(2014, 0, 1)])
@@ -158,108 +145,9 @@
                 id: id
               };
             }); //closes .map
-
-        force
-            .nodes(nodes)
-            .on("tick", tick)
-            .start().stop();
-
-        //the enter() section
-        var nodeData = svg.selectAll(".nodeG")
-            .data(nodes);
-
-        nodeData
-            .enter().append("g").attr('class', 'nodeG').append("rect")
-            .attr('class', 'node')
-            .attr("id", function(d) { return "idn-"+String(d.state).replace(/ /g,''); }) //add one id got from states (mx-state-centroids)
-          	//.attr("style", function(d) { return "fill:"+d.color+"; stroke:"+d.color+";"; })
-            //.attr("stroke", function(d) { return d.color; })
-            .attr("width", function(d) { return positive(d.r * 2); })
-            .attr("height", function(d) { return positive(d.r * 2); })
-            .attr("x", function(d) { return d.x; })
-            .attr("y", function(d) { return d.y; });
     
-        
-        nodeData.enter()
-            .append("text")
-            .attr("class", "lblValue")
-            .attr("x", function(d) { return d.x; })
-            .attr("y", function(d) { return d.y; })
-            .attr("dy", "1.4em")
-            .attr("dx", "1.6em")
-            .attr("font-family", "Helvetica")
-            .attr("font-weight", "bold")
-            .attr("font-size", function(d) { return fontSize(d.value); })
-            //.attr("fill", function(d) { return d.colorlbl;})
-            .attr("text-anchor", "middle")
-            .text(function(d){return (d.value != -1)? d.value:''});
-
-        nodeData.enter()
-              .append("text")
-              .attr("class", "lblEstado")
-              .attr("x", function(d) { return d.x; })
-              .attr("y", function(d) { return d.y; })
-              .attr("dy", "3.1em")
-              .attr("dx", "0.8em")
-              .attr("font-family", "Helvetica")
-              .attr("font-weight", "bold")
-              .attr("font-size", function(d) { return fontSize(d.value)/1.2; })
-              //.attr("fill", function(d) { return d.colorlbl;})
-              .attr("text-anchor", "start")
-              .text(function(d){return (d.value != -1)? d.state:''});
-            
         //deletes the tooltip in case it is still there.
         hideTooltip();
-
-        //for the update() section
-        var node = svg.selectAll(".node");
-        
-        node.data(nodes)
-            .on('mouseenter', function(d) {
-                borderStateGeoMap(d.state, '#000000');
-                var index = keysArray.map(function(x) {return x.state; }).indexOf(d.state);
-                chart.series[0].data[index].select();
-                showTooltip(d.state, d.value, d3.select(this).datum().x, d3.select(this).datum().y); //d3.select(this)[0][0].width.animVal.value
-            })
-            .on('mouseover', function(d) { 
-                //this.setAttribute("style", "fill:"+d.color+"; stroke:#000000; z-index:999");
-            })
-            .on('mouseleave', function(d) { 
-                //this.setAttribute("style", "fill:"+d.color+"; stroke:"+d.color);
-                var index = keysArray.map(function(x) {return x.state; }).indexOf(d.state);
-                chart.series[0].data[index].select(false);
-                hideTooltip();
-                borderStateGeoMap(d.state, '#ffffff');
-            })
-            //.attr("style", function(d) { return "fill:"+d.color+"; stroke:"+d.color+";"; })
-            .attr("x", function(d) { return d.x; })
-            .attr("y", function(d) { return d.y; })
-            //.transition().duration(500)
-            .attr("width", function(d) { return positive(d.r * 2); }) 
-            .attr("height", function(d) { return positive(d.r * 2); });
-
-        var label = svg.selectAll(".lblValue");
-        label.data(nodes)
-            .attr("x", function(d) { return d.x; })
-            .attr("y", function(d) { return d.y; })
-            .transition().duration(500)
-            .attr("font-size", function(d) { return fontSize(d.value); })
-            //.attr("fill", function(d) { return d.colorlbl;})
-            .text(function(d){return (d.value != -1)? d.value:''});
-
-        var nEstado = svg.selectAll(".lblEstado");
-         nEstado.data(nodes)
-            .attr("x", function(d) { return d.x; })
-            .attr("y", function(d) { return d.y; })
-            .transition().duration(500)
-            .attr("font-size", function(d) { return fontSize(d.value)/1.2; })
-            //.attr("fill", function(d) { return d.colorlbl;})
-            .text(function(d){return (d.value != -1)? d.state:''});
-
-        //node.data(nodes);
-        force
-            .nodes(nodes)
-            .start();
             
         GNODE = nodes; //make it available globally
         if (isMapLoaded){
@@ -270,7 +158,7 @@
             valuesArray = [];
         keysArray = [];
 
-        $.each(svg.selectAll("rect").data(), function(i, d) { //data gets an array with all the state info
+        $.each(GNODE, function(i, d) { //data gets an array with all the state info
             if (d.value != -1) //don't put in the list if no value
               keysArray.push({'state':d.state,'value':+d.value, 'color': d.color});
          });
@@ -358,7 +246,6 @@
                     events: {
                         mouseOver: function () {
                             if (String(this.category)!='Total'){
-                              //d3.select("#idn-" + String(this.category).replace(/ /g,'')).style("stroke", "black");
                               currentNodo = getNode(this.category);
                               showTooltip(this.category, currentNodo.value, currentNodo.x, currentNodo.y);
                               borderStateGeoMap(this.category, '#000000');
@@ -366,7 +253,6 @@
                         },
                         mouseOut: function () {
                             if (String(this.category)!='Total'){
-                              //d3.select("#idn-" + String(this.category).replace(/ /g,'')).style("stroke", getNode(this.category).color); //restores the fill color
                               hideTooltip();
                               borderStateGeoMap(this.category, '#ffffff');
                             }
@@ -389,65 +275,45 @@
         //console.log(Highcharts.charts);
       });
 
-        //all below is for the force layout
-        function tick(e) {
-          var grav = 1;
-          node.each(gravity(grav))
-              .each(collide(.2))
-              .attr("x", function(d) { return d.x - d.r; })
-              .attr("y", function(d) { return d.y - d.r; });
-          label.each(gravity(grav))
-              .each(collide(.2))
-              .attr("x", function(d) { return d.x - d.r; })
-              .attr("y", function(d) { return d.y - d.r; });
-          nEstado.each(gravity(grav))
-              .each(collide(.2))
-              .attr("x", function(d) { return d.x - d.r; })
-              .attr("y", function(d) { return d.y - d.r; }); 
-        }
-
-        function gravity(k) {
-          return function(d) {
-            d.x += (d.x0 - d.x) * k;
-            d.y += (d.y0 - d.y) * k;
-          };
-        }
-
-        function collide(k) {
-          var q = d3.geom.quadtree(nodes);
-          return function(node) {
-            var nr = node.r + padding,
-                nx1 = node.x - nr,
-                nx2 = node.x + nr,
-                ny1 = node.y - nr,
-                ny2 = node.y + nr;
-            q.visit(function(quad, x1, y1, x2, y2) {
-              if (quad.point && (quad.point !== node)) {
-                var x = node.x - quad.point.x,
-                    y = node.y - quad.point.y,
-                    lx = Math.abs(x),
-                    ly = Math.abs(y),
-                    r = nr + quad.point.r;
-                if (lx < r && ly < r) {
-                  if (lx > ly) {
-                    lx = (lx - r) * (x < 0 ? -k : k);
-                    node.x -= lx;
-                    quad.point.x += lx;
-                  } else {
-                    ly = (ly - r) * (y < 0 ? -k : k);
-                    node.y -= ly;
-                    quad.point.y += ly;
-                  }
-                }
-              }
-              return x1 > nx2 || x2 < nx1 || y1 > ny2 || y2 < ny1;
-            });
-          };
-        } //end function collide
+        
         
       }; //end ready (d3.json)
 
-  //add thumbnail map
+
+
+  //add geo map
+  var centroids = [{"id":"01","state":"Aguascalientes","geo_longitude":-102.2950405,"geo_latitude":21.8823971},
+{"id":"02","state":"Baja California","geo_longitude":-115.1425107,"geo_latitude":30.0338923},
+{"id":"03","state":"Baja California Sur","geo_longitude":-111.5706164,"geo_latitude":25.5818014},
+{"id":"04","state":"Campeche","geo_longitude":-90.5,"geo_latitude":19},
+{"id":"05","state":"Coahuila","geo_longitude":-102.3075153,"geo_latitude":27.2113466},
+{"id":"06","state":"Colima","geo_longitude":-104,"geo_latitude":19.166667},
+{"id":"07","state":"Chiapas","geo_longitude":-92.5000001,"geo_latitude":16.5000001},
+{"id":"08","state":"Chihuahua","geo_longitude":-106.0000001,"geo_latitude":28.5000001},
+{"id":"09","state":"Distrito Federal","geo_longitude":-99.166667,"geo_latitude":19.25},
+{"id":"10","state":"Durango","geo_longitude":-104.833333,"geo_latitude":24.33333},
+{"id":"11","state":"Guanajuato","geo_longitude":-101,"geo_latitude":21},
+{"id":"12","state":"Guerrero","geo_longitude":-100,"geo_latitude":17.666667},
+{"id":"13","state":"Hidalgo","geo_longitude":-99,"geo_latitude":20.5},
+{"id":"14","state":"Jalisco","geo_longitude":-103.6666671,"geo_latitude":20.3333331},
+{"id":"15","state":"Mexico","geo_longitude":-99.6045803,"geo_latitude":19.7253628},
+{"id":"16","state":"Michoacan","geo_longitude":-101.878113,"geo_latitude":19.707098},
+{"id":"17","state":"Morelos","geo_longitude":-99,"geo_latitude":18.75},
+{"id":"18","state":"Nayarit","geo_longitude":-105.0000001,"geo_latitude":22.3500001},
+{"id":"19","state":"Nuevo Leon","geo_longitude":-99.8873,"geo_latitude":26.2384363},
+{"id":"20","state":"Oaxaca","geo_longitude":-96.5,"geo_latitude":17},
+{"id":"21","state":"Puebla","geo_longitude":-98,"geo_latitude":18.333333},
+{"id":"22","state":"Queretaro","geo_longitude":-99.84756,"geo_latitude":21.4242575},
+{"id":"23","state":"Quintana Roo","geo_longitude":-88.8000001,"geo_latitude":19.6666671},
+{"id":"24","state":"San Luis Potosi","geo_longitude":-100.5000001,"geo_latitude":22.5000001},
+{"id":"25","state":"Sinaloa","geo_longitude":-107.5000001,"geo_latitude":25.0000001},
+{"id":"26","state":"Sonora","geo_longitude":-110.6666671,"geo_latitude":29.3333331},
+{"id":"27","state":"Tabasco","geo_longitude":-92.6666671,"geo_latitude":18.0000001},
+{"id":"28","state":"Tamaulipas","geo_longitude":-98.7500001,"geo_latitude":24.0000001},
+{"id":"29","state":"Tlaxcala","geo_longitude":-97.23671533,"geo_latitude":19.3180485},
+{"id":"30","state":"Veracruz","geo_longitude":-95.666667,"geo_latitude":19.733333},
+{"id":"31","state":"Yucatan","geo_longitude":-89.09984681,"geo_latitude":20.5878917},
+{"id":"32","state":"Zacatecas","geo_longitude":-103.0000001,"geo_latitude":23.3000001}];
   // initialize qtip tooltip class
   $.fn.qtip.defaults.style.classes = 'ui-tooltip-bootstrap';
   $.fn.qtip.defaults.style.def = false;
@@ -476,7 +342,6 @@
           },
           mouseenter: function(d, path) {
               path.attr('stroke', '#000000');
-              d3.select("#idn-" + String(d.name).replace(/ /g,'')).style("stroke", "black");  
               var index = keysArray.map(function(x) {return x.state; }).indexOf(d.name);
               chart.series[0].data[index].select();
               currentNodo = getNode(d.name);
@@ -484,16 +349,55 @@
           }, 
           mouseleave: function(d, path) {
               path.attr('stroke', '#ffffff'); 
-              d3.select("#idn-" + String(d.name).replace(/ /g,'')).style("stroke", getNode(d.name).color); //restores the last color
               var index = keysArray.map(function(x) {return x.state; }).indexOf(d.name);
               chart.series[0].data[index].select(false);
               hideTooltip();
           }
       });
 
+      map.addSymbols({
+            type: kartograph.Label,
+            data: centroids,
+            location: function(d) { return [d.geo_longitude, d.geo_latitude] },
+            text: function(d) { return d.state + " " +getNode(d.state).value; },
+            mouseenter: function(d, path) {
+              currentNodo = getNode(d.state);
+              borderStateGeoMap(d.state, '#000000');
+              var index = keysArray.map(function(x) {return x.state; }).indexOf(d.state);
+              chart.series[0].data[index].select();
+              showTooltip(d.state, currentNodo.value, currentNodo.x, currentNodo.y);
+            }, 
+            mouseleave: function(d, path) {
+              borderStateGeoMap(d.state, '#ffffff');
+              var index = keysArray.map(function(x) {return x.state; }).indexOf(d.state);
+              chart.series[0].data[index].select(false);
+              hideTooltip();
+            }
+      });
+      
       updateMap = function() {
           map.getLayer('admin1')
             .style('fill', function(d) { return getNode(d.name).color;});
+
+          map.removeSymbols();
+          map.addSymbols({
+            type: kartograph.Label,
+            data: centroids,
+            location: function(d) { return [d.geo_longitude, d.geo_latitude] },
+            text: function(d) { return d.state + " " +getNode(d.state).value; },
+            mouseenter: function(d, path) {
+              currentNodo = getNode(d.state);
+              borderStateGeoMap(d.state, '#000000');
+              var index = keysArray.map(function(x) {return x.state; }).indexOf(d.state);
+              chart.series[0].data[index].select();
+              showTooltip(d.state, currentNodo.value, currentNodo.x, currentNodo.y);
+            }, 
+            mouseleave: function(d, path) {
+              borderStateGeoMap(d.state, '#ffffff');
+              var index = keysArray.map(function(x) {return x.state; }).indexOf(d.state);
+              chart.series[0].data[index].select(false);
+              hideTooltip();
+            }});
       }
 
       borderStateGeoMap = function (name, color){
